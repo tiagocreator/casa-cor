9641dd0ed026e4fd575fc687096a8024f13bca31
Tiago Leite <contato@tiagocreator.com>
2023-10-02 14:04:24 -0300

    Initial commit

---
+++ css/nav.js
@@ -0,0 +1,6 @@
+const navbarMenu = document.querySelector('#navigation #navbar-menu');
+const hamburgerMenu = document.querySelector('#navigation .hamburger-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+  navbarMenu.classList.toggle('open');
+});
